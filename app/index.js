2cc6b0db7da3c4454b6315899fe344bc9b54bb09
CERTIFIED2003 <111045472+CERTIFIED2003@users.noreply.github.com>
2023-04-05 20:26:24 +0530

    Component structures created

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";  // View acts as a <div /> & Text is like a <p />
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+export default Home;
